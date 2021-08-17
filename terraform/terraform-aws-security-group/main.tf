2f612150abdae2edd5f23946281720e2a0db8fc8
Inderpreet Singh <indercodes@gmail.com>
2021-08-17 11:51:00 +0530

    initial commit

---
+++ terraform/terraform-aws-security-group/main.tf
@@ -0,0 +1,46 @@
+terraform {
+  backend "s3" {
+    bucket = "cspm-terraform-state"
+    key    = "terraform/terraform-aws-security-group"
+    region = "ap-south-1"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "vpc" {
+  source      = "clouddrove/vpc/aws"
+  version     = "0.15.0"
+  name        = "vpc"
+  environment = "test"
+  label_order = ["name", "environment"]
+  cidr_block  = "10.0.0.0/16"
+}
+
+module "security_group_tcp" {
+  source        = "clouddrove/security-group/aws"
+  version       = "0.15.0"
+  name          = "security-group"
+  environment   = "tcp"
+  protocol      = "tcp"
+  label_order   = ["name", "environment"]
+  vpc_id        = module.vpc.vpc_id
+  allowed_ip    = ["0.0.0.0/0"]
+  allowed_ipv6  = ["::/0"]
+  allowed_ports = [20, 21, 22, 23, 25, 445, 1433, 3389, 4333, 3306, 5432, 5500, 5900, 1521, 5601, 8020, 50070, 50470, 4505, 4506, 2375, 2376]
+}
+
+module "security_group_udp" {
+  source        = "clouddrove/security-group/aws"
+  version       = "0.15.0"
+  name          = "security-group"
+  environment   = "udp"
+  protocol      = "tcp"
+  label_order   = ["name", "environment"]
+  vpc_id        = module.vpc.vpc_id
+  allowed_ip    = ["0.0.0.0/0"]
+  allowed_ipv6  = ["::/0"]
+  allowed_ports = [53, 135, 137, 138]
+}
